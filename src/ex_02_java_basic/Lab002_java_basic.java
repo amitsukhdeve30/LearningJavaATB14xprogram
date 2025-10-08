af355a26d96f266f4f223960cd03913cd7b77e35
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2025-10-08 12:36:26 +0530

    Lab002_java_basic

---
+++ src/ex_02_java_basic/Lab002_java_basic.java
@@ -0,0 +1,10 @@
+package ex_02_java_basic;
+
+public class Lab002_java_basic {
+    public static void main(String[] args) {
+        System.out.println("hello world 1");
+        System.out.println("hello world 2");
+        System.out.println("hello world 3");
+        System.out.println("hello world 4");
+    }
+}
