5e6a9bc7c88a6345de04e3e6d819c7bf10e70acf
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2025-11-20 16:38:00 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/ex_02_java_basic/Lab010_printF.java
@@ -0,0 +1,27 @@
+package ex_02_java_basic;
+
+public class Lab010_printF {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//
+//        System.out.print(a);
+//        System.out.print(b);
+
+        System.out.printf("value of a=%d ", a);
+        System.out.println();
+        System.out.printf("value of b =%d", b);
+
+//        %d -> int , byte , long , short , - data type
+//        %s -> String
+//        %f ->float , double
+//        %b ->boolean
+
+
+    }
+}
+
