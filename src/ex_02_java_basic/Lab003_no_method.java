ffdfb4ae3fe9225f924ce2bc9e4b9fb7631be5f0
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2025-11-20 11:48:16 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

--- src/ex_02_java_basic/Lab003_no_method.java
+++ src/ex_02_java_basic/Lab003_no_method.java
@@ -1,4 +1,8 @@
 package ex_02_java_basic;
 
 public class Lab003_no_method {
+    //Program to explain that we can create a class without main method
+    // yes we can create a class without main method.
+    // Here compiler doesn't show any error.
+    // But we cannot run this code because JVM LOOKS FOR MAIN METHOD TO EXECUTE CODE
 }
---
+++ src/ex_02_java_basic/Lab004_comments.java
@@ -0,0 +1,29 @@
+package ex_02_java_basic;
+
+public class Lab004_comments {
+
+//    This is comment , this code will be ignored by the compiler
+    //2+2
+
+
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+    //This is single line comments
+
+
+
+
+
+
+    /* this is multiple line comments
+     it can cover multiple lines
+     useful for long explanations
+     */
+}
