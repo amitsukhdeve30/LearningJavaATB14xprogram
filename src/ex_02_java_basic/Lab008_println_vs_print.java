2ac2bcd00dabe4ce8f9c348cd35af6a8ce7aed7a
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2025-11-20 12:53:30 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/ex_02_java_basic/Lab008_println_vs_print.java
@@ -0,0 +1,13 @@
+package ex_02_java_basic;
+
+public class Lab008_println_vs_print {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("bye");
+//        new line
+
+
+        System.out.print("hi");
+        System.out.print("bye");
+    }
+}
