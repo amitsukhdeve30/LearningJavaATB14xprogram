fc226e99128d310a17ce7eb76dc0c245ca13e15e
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2025-11-20 12:44:04 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

--- src/ex_02_java_basic/Lab005_Keywords_Identifiers.java
+++ src/ex_02_java_basic/Lab005_Keywords_Identifiers.java
@@ -1,4 +1,5 @@
 package ex_02_java_basic;
+public class Lab005_Keywords_Identifiers {
 
 public static void main(String[] args) {
     System.out.println("hello world");
---
+++ src/ex_02_java_basic/Lab006_Common_math.java
@@ -0,0 +1,12 @@
+package ex_02_java_basic;
+
+public class Lab006_Common_math {
+    public static void main(String[] args) {
+
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println("2+2");
+    }
+}
