5a1a481fe3d2b486e8c281c948461a67c494f0bf
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2025-11-20 16:50:36 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/ex_02_java_basic/Lab011_table9.java
@@ -0,0 +1,17 @@
+package ex_02_java_basic;
+
+public class Lab011_table9 {
+    public static void main(String[] args) {
+        int table = 9;
+        System.out.printf("%d*1=%d ", table , table*1);
+        System.out.printf("%d*2=%d ", table , table*2);
+        System.out.printf("%d*3=%d ", table , table*3);
+        System.out.printf("%d*4=%d ", table , table*4);
+        System.out.printf("%d*5=%d ", table , table*5);
+        System.out.printf("%d*6=%d ", table , table*6);
+        System.out.printf("%d*7=%d ", table , table*7);
+        System.out.printf("%d*8=%d ", table , table*8);
+        System.out.printf("%d*9=%d ", table , table*9);
+        System.out.printf("%d*10=%d ", table , table*10);
+    }
+}
