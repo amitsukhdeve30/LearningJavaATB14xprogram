a08c034db4d885da1dacb051c3bd1d92a94c09fc
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2026-01-22 17:06:31 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/ex_02_java_basic/Lab012_local_variables.java
@@ -0,0 +1,16 @@
+package ex_02_java_basic;
+
+public class Lab012_local_variables {
+    public static void main(String[] args) {
+        int a = 10;
+        byte b = 10;
+        short s = 10;
+        long l = 1234567899;
+        float f = 3.145F;
+        double d = 18.6999997;
+        boolean b1 = true;
+        char c ='A';
+
+
+    }
+}
