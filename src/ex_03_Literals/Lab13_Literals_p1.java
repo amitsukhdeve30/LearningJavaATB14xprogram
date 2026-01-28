420d82dadde19b5c79b0bef7a2e34b8ebc027a4f
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2026-01-28 15:41:33 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/ex_03_Literals/Lab13_Literals_p1.java
@@ -0,0 +1,12 @@
+package ex_03_Literals;
+
+public class Lab13_Literals_p1 {
+    int age = 65;
+    //literals = integral
+
+    final float pi = 3.15f;
+    //Literals = float
+
+    final int girl_age= 18;
+    //girl age =19;
+}
