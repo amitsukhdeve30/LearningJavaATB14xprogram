049c24cdfbbccbc26f198cd77cad49784b9b965d
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2026-01-31 14:49:52 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/ex_03_Literals/Lab15_Boolean_Literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Lab15_Boolean_Literals {
+
+    public static void main(String[] args) {
+
+        Boolean is_married =true;  //valid boolean literals
+        Boolean is_married_sham =false; // valid boolean literals
+        Boolean is_married_sham = 1123 ; //invalid boolean literals
+    }
+}
