65c01ce7568ce4847210af25222969e9895154eb
Amit Sukhdeve <amitsukhdeve30@gmail.com>
2026-02-11 12:48:18 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/ex_01_Basic_java/Lab001_HelloWorld.java

---
+++ src/Operators/Lab19_Operators.java
@@ -0,0 +1,12 @@
+package Operators;
+
+public class Lab19_Operators {
+    int age = 90;
+    // Assignment operator
+    // int -> data type
+    // age -> identifier
+    // = -> Assignment operator
+    // 90 -> Literal
+
+    // RHV to the LH
+}
